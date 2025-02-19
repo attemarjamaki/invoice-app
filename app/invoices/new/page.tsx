9ab18fba37cd685313e57dcbf3ee428ca73cbbf3
unknown <atte.marjamaki@arcada.fi>
2025-02-19 20:49:20 +0200

    adding invoices to db

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,20 +1,36 @@
-import { sql } from "drizzle-orm";
-import { db } from "@/db";
-import { Button } from "@/components/ui/button";
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { formAction } from "@/app/actions";
+import { SyntheticEvent, useState, startTransition } from "react";
+import SubmitButton from "@/components/SubmitButton";
 
-export default async function NewInvoice() {
-  const results = await db.execute(sql`SELECT current_database()`);
+export default function Page() {
+  const [state, setState] = useState("ready");
+  async function handleOnSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+    if (state === "pending") return;
+    setState("pending");
+    const target = e.target as HTMLFormElement;
+    startTransition(async () => {
+      const formData = new FormData(target);
+      await formAction(formData);
+      console.log("hey");
+    });
+  }
   return (
     <main className="flex flex-col justify-center gap-6 max-w-5xl mx-auto py-12 px-2">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Create Invoice</h1>
       </div>
 
-      {JSON.stringify(results)}
-      <form className="grid gap-4 max-w-sm">
+      <form
+        action={formAction}
+        onSubmit={handleOnSubmit}
+        className="grid gap-4 max-w-sm"
+      >
         <div>
           <Label htmlFor="name" className="block font-semibold text-sm mb-2">
             Billing Name
@@ -43,7 +59,7 @@ export default async function NewInvoice() {
           <Textarea id="description" name="description" />
         </div>
         <div>
-          <Button className="w-full font-semibold">Submit</Button>
+          <SubmitButton />
         </div>
       </form>
     </main>
